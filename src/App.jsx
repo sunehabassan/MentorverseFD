cf28c3df45a2c08c8c9e4ade0113dc6a5ec7cfa2
suneha <sunehabassan9@gmail.com>
2025-05-23 21:40:11 +0530

    ERROR solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
 
-  return (
-    <>
+//   return (
+//     <>
       
-      <div class="text-3xl font-bold underline">hello</div>
-    </>
-  )
-}
+//       <div class="text-3xl font-bold underline">hello</div>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
